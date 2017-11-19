0236cc9322d0aa975db45d59c71440d5de0658b5
Felipe D Quinones <felipe.dj.quinones@gmail.com>
2017-11-19 16:38:54 -0700

    added validate function code

--- Asig_Funciones/asig_funciones.cpp
+++ Asig_Funciones/asig_funciones.cpp
@@ -3,3 +3,27 @@ Crear una funcion que valide la siguiente data
 s >0  and s <=90 or s>210 and s<230  entonce valor bueno, sino valor malo.
 */
 
+#include <iostream>
+using namespace std;
+
+int validate();
+
+int main()
+{
+
+}
+
+int validate()
+{
+	int data;
+	do
+	{
+		cout << "Enter data: ";
+		cin >> data;
+		if (!((data > 0 && data <= 90) || (data > 210 && data < 230)))
+		{
+			cout << "Error try again " << endl;
+		}while ((data > 90 || data < 210) && data > 230)
+			return data;
+	}
+}
